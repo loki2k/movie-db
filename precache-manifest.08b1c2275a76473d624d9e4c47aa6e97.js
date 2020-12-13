4a183769cd27d6859c1ca0176bc8e0a153639e23
loki2k <jeremylagarto@gmail.com>
2020-12-13 11:30:11 -0800

    Updates

---
+++ precache-manifest.08b1c2275a76473d624d9e4c47aa6e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97b43331abe88b5f437589fcad6bb0d",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "f7809598f116031b7a6a",
+    "url": "/movie-db/static/css/main.3d22e596.chunk.css"
+  },
+  {
+    "revision": "9aff709ffce6dd2dd0a7",
+    "url": "/movie-db/static/js/2.5b374e73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-db/static/js/2.5b374e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7809598f116031b7a6a",
+    "url": "/movie-db/static/js/main.c7dd06ac.chunk.js"
+  },
+  {
+    "revision": "27447de3e1d8ded13550",
+    "url": "/movie-db/static/js/runtime-main.b7c080ae.js"
+  }
+]);
